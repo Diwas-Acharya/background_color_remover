623d7600b30ca1456821e3acc64544b2bd414a4a
Diwas Acharya <47555075+Diwas-Acharya@users.noreply.github.com>
2020-06-15 00:01:17 -0700

    Add files via upload

---
+++ background_color_remover/background_colour_changer.py
@@ -0,0 +1,25 @@
+
+import numpy as np 
+import cv2 
+import imutils
+  
+img = cv2.imread('mini.jpg')
+
+img = imutils.resize(img , width = 480)
+img_array = np.array(img)
+shape = img_array.shape
+
+# This program removes Green color 
+u_green = np.array([40, 150, 10])
+l_green = np.array([21,255,15])
+
+# Specify a rgb color code for your color
+
+#looping throung all the pixels in the image if pixels is green in color then just it by black color
+for x in range(shape[0]):
+	for y in range(shape[1]):
+		if list(img[x][y]) < list(u_green) or list(img[x][y]) == list(l_green):
+			img[x][y] = 0
+
+cv2.imshow('image', img) 
+k = cv2.waitKey(0) & 0xFF  
