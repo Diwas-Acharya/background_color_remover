fbe7455b1cdd9552197af46bf775811b93eb1290
Diwas Acharya <47555075+Diwas-Acharya@users.noreply.github.com>
2020-10-02 11:21:26 -0400

    Update background_colour_changer.py

--- background_color_remover/background_colour_changer.py
+++ background_color_remover/background_colour_changer.py
@@ -9,13 +9,14 @@ img = imutils.resize(img , width = 480)
 img_array = np.array(img)
 shape = img_array.shape
 
-# This program removes Green color 
+# This program removes Green color in the background
+
 u_green = np.array([40, 150, 10])
 l_green = np.array([21,255,15])
 
 # Specify a rgb color code for your color
 
-#looping throung all the pixels in the image if pixels is green in color then just it by black color
+#looping throung all the pixels of the image if pixels is green in color then replace with black color
 for x in range(shape[0]):
 	for y in range(shape[1]):
 		if list(img[x][y]) < list(u_green) or list(img[x][y]) == list(l_green):
